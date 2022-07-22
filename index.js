1aee4216df1bafc8b6d33dc3ba547a8a20120183
Naoya Takamatsu <takamatsu@shiftplus.jp>
2022-07-22 13:35:43 +0900

    関数分割

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
+// DOM 取得
 const button = document.getElementById("addBtn");
 const lists = document.getElementById("lists")
 
-async function listUsers() {
-    // データのやり取り
-    const res = await fetch("https://jsonplaceholder.typicode.com/users");
-    const users = await res.json();
-
-    // DOM操作
-    users.forEach(function(user){
-        const list = document.createElement("li");
-        list.innerText = user.name;
-        lists.appendChild(list)
-    });
+// DOM 操作(関数)
+function addList(user){
+    const list = document.createElement("li");
+    list.innerText = user.name;
+    lists.appendChild(list)
 };
 
-window.addEventListener("load", listUsers());
-button.addEventListener("click", listUsers());
+// データのやり取り(非同期関数)
+async function getUsers() {
+    const res = await fetch("https://jsonplaceholder.typicode.com/users");
+    const users = await res.json();
+    return users
+};
+
+// (非同期関数)
+async function listUsers() {
+    // データのやり取り
+    const users = await getUsers();
+    // DOM操作
+    users.forEach(addList);
+};
+
+// イベント
+window.addEventListener("load", listUsers);
+button.addEventListener("click", listUsers);
