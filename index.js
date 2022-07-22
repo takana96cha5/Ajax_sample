465aa0c73bb5fe488b33e247f961fdad33b8625d
Naoya Takamatsu <takamatsu@shiftplus.jp>
2022-07-22 11:57:43 +0900

    js 非同期処理 練習

---
+++ index.js
@@ -0,0 +1,28 @@
+const button = document.getElementById("addBtn");
+const lists = document.getElementById("lists")
+
+button.addEventListener("click", async function() {
+    // データのやり取り
+    const res = await fetch("https://jsonplaceholder.typicode.com/users");
+    const users = await res.json();
+
+    // DOM操作
+    users.forEach(function(user){
+        const list = document.createElement("li");
+        list.innerText = user.name;
+        lists.appendChild(list)
+    });
+});
+
+window.addEventListener("load", async function (){
+    // データのやり取り
+    const res = await fetch("https://jsonplaceholder.typicode.com/users");
+    const users = await res.json();
+
+    // DOM操作
+    users.forEach(function(user){
+        const list = document.createElement("li");
+        list.innerText = user.name;
+        lists.appendChild(list)
+    });
+});
