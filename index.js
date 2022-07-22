63cd2c37f35eed9ac6af3ec3a03025bfb7b04ec7
Naoya Takamatsu <takamatsu@shiftplus.jp>
2022-07-22 12:01:13 +0900

    ユーザーのリスト化処理を関数にまとめた

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const button = document.getElementById("addBtn");
 const lists = document.getElementById("lists")
 
-button.addEventListener("click", async function() {
+async function listUsers() {
     // データのやり取り
     const res = await fetch("https://jsonplaceholder.typicode.com/users");
     const users = await res.json();
@@ -12,17 +12,7 @@ button.addEventListener("click", async function() {
         list.innerText = user.name;
         lists.appendChild(list)
     });
-});
+};
 
-window.addEventListener("load", async function (){
-    // データのやり取り
-    const res = await fetch("https://jsonplaceholder.typicode.com/users");
-    const users = await res.json();
-
-    // DOM操作
-    users.forEach(function(user){
-        const list = document.createElement("li");
-        list.innerText = user.name;
-        lists.appendChild(list)
-    });
-});
+window.addEventListener("load", listUsers());
+button.addEventListener("click", listUsers());
